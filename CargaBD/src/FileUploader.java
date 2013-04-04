fadb341cc717bb8ad020b972728b831cb7e04f10
Mario Leverone <mleve@ug.uchile.cl>
2013-04-04 14:26:56 -0300

    Parser funcionando bien

--- CargaBD/src/FileUploader.java
+++ CargaBD/src/FileUploader.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 
 public class FileUploader {
@@ -13,6 +14,7 @@ public class FileUploader {
 	public static void fileUploader(String filename){
 		FileReader fr;
 		BufferedReader reader;
+		StringTokenizer separator = null;
 		
 		try {
 			fr = new FileReader(filename);
@@ -22,9 +24,14 @@ public class FileUploader {
 		System.out.println("File content:");
 		while ((actualLine = reader.readLine()) != null)   {
 			  // Print the content on the console
+			separator = new StringTokenizer(actualLine,",");
 			
-			System.out.println (actualLine);
+			while(separator.hasMoreTokens()){
+				System.out.print(separator.nextToken()+"\t");
+			}
+			System.out.println("");
 		}
+		
 		} catch (Exception e) {
 		// TODO Auto-generated catch block
 		System.out.println("Error al abrir el archivo de input");
